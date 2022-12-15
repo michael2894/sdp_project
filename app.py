581b62c4ce72f2e18f83f2adae591f0eee43f21e
michael2894 <pm4@gmx.at>
2022-12-15 10:33:13 +0100

    changes for github actions test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, SDP class!</p>"
+    return "<p>Hello, Huberk Hubert du heisl!</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
