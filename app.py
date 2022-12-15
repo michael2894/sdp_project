3c84481eb78ccb4f7faf2328b47e45cf414c9c42
HubwerkHubert <metti93@gmail.com>
2022-12-15 16:12:42 +0100

    updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Hubwerk Hubert du heisl! Michaels, du scheißküwe</p>"
+    return "<p>Hello, Hubwerk Hubert du heisl! Michaels, du scheißküwe!!</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
